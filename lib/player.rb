d9c10e4b8b3fa76db0c3dbb7383805d34a88d2a4
Adam G <adamwizzy@hotmail.co.uk>
2020-11-11 16:14:23 +0000

    Added hp and attack to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,13 @@
 class Player
   attr_reader :name
+  attr_accessor :hp
 
   def initialize(name)
     @name = name
+    @hp = 100
+  end
+
+  def attack(other_player)
+    other_player.hp -= 10
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,9 +2,14 @@ require 'player'
 
 describe Player do
   subject { Player.new("Dingus") }
+  let(:doofus) { Player.new("Doofus") }
 
   it "Returns it's name" do
     expect(subject.name).to eq("Dingus")
+  end
+
+  it "Being hit by an attack reduces health by 10" do
+    expect { subject.attack(doofus) }.to change { doofus.hp }.by(-10)
   end
 
 end
